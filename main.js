dc722820de29eb97e6571d8f45407c8fbc1e9801
Chanchakornj <chanchakornj@gmail.com>
2026-07-05 18:04:52 +0700

    4.9 add onSale attribute

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ createApp({
         const description = ref('Comfortable footwear')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = ref(true)
 
         return{
             product,
             image,
             description,
-            link
+            link,
+            inStock,
+            inventory,
+            onSale
         }
 
         
