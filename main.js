65ea209a3cb99b8750a084d10d78a8d50413b3aa
ChanchakornJ <chanchakornj@gmail.com>
2026-07-05 23:08:30 +0700

    8.5 add onSale

--- main.js
+++ main.js
@@ -1,37 +1,60 @@
-const {createApp, ref} = Vue
+const {createApp, ref, computed} = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
+        const brand = ref('SE 331')
         const description = ref('Comfortable footwear')
-        const image = ref('./assets/images/socks_green.jpg')
+      
         const link = ref('https://www.camt.cmu.ac.th')
-        const inStock = ref(true)
+      
         const inventory = ref(100)
-        const onSale = ref(true)
+       
         const details = ref([
             '50% cotton',
             '30% wool',
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green', size: 'S,M,L', image:'./assets/images/socks_green.jpg'},
-            {id: 2235, color: 'blue',  size: 'S,M,L',  image:'./assets/images/socks_blue.jpg'}
+            {id: 2234, color: 'green', size: 'S,M,L', image:'./assets/images/socks_green.jpg', quantity: 50 , onSale: true},
+            {id: 2235, color: 'blue',  size: 'S,M,L',  image:'./assets/images/socks_blue.jpg', quantity: 0, onSale: false}
         ])
+        const selectedVariant = ref(0)
         const cart = ref(0)
         
         function addToCart(){
             cart.value +=1
         }
-        function  updateImage(variantImage){
-            image.value = variantImage
-        }
+        const title = computed(() =>{
+            let title = brand.value + ' ' + product.value 
+
+            if(onSale.value){
+                title += ' is on sale'
+            }
+
+            return title
+        })
+     
         function toggleStock(){
             inStock.value = !inStock.value
         }
+        function updateVariant(index){
+            selectedVariant.value = index;
+        }
+
+        const image = computed(() =>{
+            return variants.value[selectedVariant.value].image
+        })
+         const inStock = computed(() =>{
+            return variants.value[selectedVariant.value].quantity
+        }) 
+
+       const onSale = computed(() => {
+            return variants.value[selectedVariant.value].onSale
+        })
 
         return{
-            product,
+            title,
             image,
             description,
             link,
@@ -42,8 +65,8 @@ createApp({
             variants,
             cart,
             addToCart,
-            updateImage,
-            toggleStock
+            toggleStock,
+            updateVariant
         }
 
         
