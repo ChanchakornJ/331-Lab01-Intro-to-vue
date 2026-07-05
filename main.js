818b8273fab65546159b864149be0127db846284
ChanchakornJ <Chanchakornj@gmail.com>
2026-07-05 14:54:31 +0700

    3.6 add a link to product name

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ createApp({
     setup(){
         const product = ref('Boots')
         const description = ref('Comfortable footwear')
+        const image = ref('./assets/images/socks_green.jpg')
+        const link = ref('https://www.camt.cmu.ac.th')
 
         return{
             product,
-            description
+            image,
+            description,
+            link
         }
 
         
