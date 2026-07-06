a5e4c2146a4b28de7f8090d3f213df70513dd9f1
ChanchakornJ <Chanchakornj@gmail.com>
2026-07-06 14:12:20 +0700

    12.3 updated source code using deconstructor

--- components/ReviewForm.js
+++ components/ReviewForm.js
@@ -4,19 +4,19 @@ const reviewForm = {
     `<form class="review-form" @submit.prevent="onSubmit">
     <h3>Leave a review</h3>
     <label for="name">Name:</label>
-    <input id="name" v-model="form.name">
+    <input id="name" v-model="name">
 
     <label for="review">Review:</label>
-    <textarea id="review" v-model="form.review"></textarea>
+    <textarea id="review" v-model="review"></textarea>
 
      <label for="review">Would you recommend this product?: </label>
-    <select id="recommend" v-model="form.recommend">
+    <select id="recommend" v-model="recommend">
       <option>Yes</option>
       <option>No</option>
     </select>
 
     <label for="rating">Rating:</label>
-    <select id="rating" v-model.number="form.rating">
+    <select id="rating" v-model.number="rating">
       <option>5</option>
       <option>4</option>
       <option>3</option>
@@ -55,7 +55,7 @@ const reviewForm = {
     }
 
     return{
-        form,
+        ...toRefs(form),
         onSubmit
     }
   }
